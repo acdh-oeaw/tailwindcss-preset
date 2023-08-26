d3027f1d27d6cfa90fe005d885acf5804646ebe5
Stefan Probst <stefanprobst@posteo.de>
2023-08-26 18:10:13 +0200

    fix: accept breakpoints via options param

--- src/preset.ts
+++ src/preset.ts
@@ -2,10 +2,13 @@ import typographyPlugin from "@tailwindcss/typography";
 import { type Config } from "tailwindcss";
 import animatePlugin from "tailwindcss-animate";
 
-import { plugin as designTokensPlugin } from "./plugin.js";
+import { createPlugin } from "./plugin.js";
+import { type Options } from "./types.js";
 
-export const preset = {
-	content: [],
-	darkMode: ["class", '[data-color-scheme="dark"]'],
-	plugins: [animatePlugin, designTokensPlugin, typographyPlugin],
-} satisfies Config;
+export function createPreset(options?: Options) {
+	return {
+		content: [],
+		darkMode: ["class", '[data-color-scheme="dark"]'],
+		plugins: [animatePlugin, typographyPlugin, createPlugin(options)],
+	} satisfies Config;
+}
